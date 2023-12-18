e444db24993c0a2ef1be988351bb978dd5879ed0
Tan <ngoctana1k2014@gmail.com>
2023-12-18 11:11:07 +0700

    add task schedule

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import StellarSdk from '@stellar/stellar-sdk';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private server: any;
+  constructor() {
+    StellarSdk.Network.useTestNetwork();
+    this.server = new StellarSdk.Server('https://horizon-testnet.stellar.org');
   }
 }
