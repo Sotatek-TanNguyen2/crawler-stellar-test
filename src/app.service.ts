fa2d3799aa2b1ac86d42de4bb741aaa93992d392
Tan <ngoctana1k2014@gmail.com>
2023-12-22 09:15:01 +0700

    test send transaction by horizon

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,10 +1,13 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable, Logger } from '@nestjs/common';
 import { Timeout } from '@nestjs/schedule';
 import {
+  Asset,
   Contract,
   Horizon,
   Keypair,
   Networks,
+  Operation,
   SorobanRpc,
   TransactionBuilder,
   xdr,
@@ -26,7 +29,7 @@ export class AppService {
   private readonly horizonServer: Horizon.Server;
   private readonly sorobanServer: SorobanRpc.Server;
   private currentLedger: number;
-  constructor() {
+  constructor(private readonly httpService: HttpService) {
     this.horizonServer = new Horizon.Server(HORIZON_FUTURE);
     this.sorobanServer = new SorobanRpc.Server(FUTURE_RPC, {
       allowHttp: true,
@@ -36,7 +39,7 @@ export class AppService {
     this.blockPerJob = 20;
   }
 
-  @Timeout(0)
+  // @Timeout(0)
   async decodeXdr() {
     const xdr_encoded = {
       envelope_xdr:
@@ -74,119 +77,144 @@ export class AppService {
   }
 
   // @Timeout(0)
-  // async sendTransaction() {
-  //   const contract = new Contract(CONTRACT_ID);
-  //   const sourceKeypair = Keypair.fromSecret(SECRET_KEY);
-  //   const sourceAccount = await this.sorobanServer.getAccount(
-  //     sourceKeypair.publicKey(),
-  //   );
-  //   const transaction = new TransactionBuilder(sourceAccount, {
-  //     fee: '100',
-  //     networkPassphrase: Networks.FUTURENET,
-  //   })
-  //     .addOperation(contract.call('hello', ...[xdr.ScVal.scvSymbol('world')]))
-  //     .setTimeout(300)
-  //     .build();
+  async sendTransactionHorizon() {
+    try {
+      const contract = new Contract(CONTRACT_ID);
+      const account = await this.horizonServer.loadAccount(ACCOUNT_ID);
+      const fee = await this.horizonServer.fetchBaseFee();
+      const transaction = new TransactionBuilder(account, {
+        fee: fee.toString(),
+        networkPassphrase: Networks.FUTURENET,
+      })
+        .addOperation(contract.call('hello', ...[xdr.ScVal.scvString('world')]))
+        .setTimeout(300)
+        .build();
 
-  //   const preparedTransaction =
-  //     await this.sorobanServer.prepareTransaction(transaction);
+      transaction.sign(Keypair.fromSecret(SECRET_KEY));
 
-  //   preparedTransaction.sign(sourceKeypair);
-  //   const sendResponse =
-  //     await this.sorobanServer.sendTransaction(preparedTransaction);
-
-  //   console.log(`Sent transaction: ${JSON.stringify(sendResponse)}`);
-  //   if (sendResponse.status === 'PENDING') {
-  //     let getResponse = await this.sorobanServer.getTransaction(
-  //       sendResponse.hash,
-  //     );
-
-  //     while (getResponse.status === 'NOT_FOUND') {
-  //       console.log('Waiting for transaction confirmation...');
-  //       // See if the transaction is complete
-  //       getResponse = await this.sorobanServer.getTransaction(
-  //         sendResponse.hash,
-  //       );
-  //       // Wait one second
-  //       await new Promise((resolve) => setTimeout(resolve, 1000));
-  //     }
-
-  //     console.log(`getTransaction response: ${JSON.stringify(getResponse)}`);
-
-  //     if (getResponse.status === 'SUCCESS') {
-  //       // Make sure the transaction's resultMetaXDR is not empty
-  //       if (!getResponse.resultMetaXdr) {
-  //         throw 'Empty resultMetaXDR in getTransaction response';
-  //       }
-  //       // Find the return value from the contract and return it
-  //       let transactionMeta = getResponse.resultMetaXdr;
-  //       let returnValue = transactionMeta.v3().sorobanMeta()!.returnValue();
-  //       console.log(`Transaction result: ${returnValue.value()}`);
-  //     } else {
-  //       throw `Transaction failed: ${getResponse}`;
-  //     }
-  //   }
-  // }
+      const transactionResult =
+        await this.horizonServer.submitTransaction(transaction);
+      this.logger.log(transactionResult);
+    } catch (error) {
+      this.logger.error(error);
+    }
+  }
 
   // @Timeout(0)
-  // async getTransactionsByAcconut() {
-  //   try {
-  //     const data = await this.horizonServer
-  //       .transactions()
-  //       .forAccount(ACCOUNT_ID)
-  //       .call();
-  //     this.logger.log(JSON.stringify(data));
-  //   } catch (error) {
-  //     this.logger.error(error);
-  //   }
-  // }
+  async sendTransactionSoroban() {
+    const contract = new Contract(CONTRACT_ID);
+    const sourceKeypair = Keypair.fromSecret(SECRET_KEY);
+    const sourceAccount = await this.sorobanServer.getAccount(
+      sourceKeypair.publicKey(),
+    );
+    const transaction = new TransactionBuilder(sourceAccount, {
+      fee: '100',
+      networkPassphrase: Networks.FUTURENET,
+    })
+      .addOperation(contract.call('hello', ...[xdr.ScVal.scvSymbol('world')]))
+      .setTimeout(300)
+      .build();
+
+    const preparedTransaction =
+      await this.sorobanServer.prepareTransaction(transaction);
+
+    preparedTransaction.sign(sourceKeypair);
+    const sendResponse =
+      await this.sorobanServer.sendTransaction(preparedTransaction);
+
+    console.log(`Sent transaction: ${JSON.stringify(sendResponse)}`);
+    if (sendResponse.status === 'PENDING') {
+      let getResponse = await this.sorobanServer.getTransaction(
+        sendResponse.hash,
+      );
+
+      while (getResponse.status === 'NOT_FOUND') {
+        console.log('Waiting for transaction confirmation...');
+        // See if the transaction is complete
+        getResponse = await this.sorobanServer.getTransaction(
+          sendResponse.hash,
+        );
+        // Wait one second
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+      }
+
+      console.log(`getTransaction response: ${JSON.stringify(getResponse)}`);
+
+      if (getResponse.status === 'SUCCESS') {
+        // Make sure the transaction's resultMetaXDR is not empty
+        if (!getResponse.resultMetaXdr) {
+          throw 'Empty resultMetaXDR in getTransaction response';
+        }
+        // Find the return value from the contract and return it
+        let transactionMeta = getResponse.resultMetaXdr;
+        let returnValue = transactionMeta.v3().sorobanMeta()!.returnValue();
+        console.log(`Transaction result: ${returnValue.value()}`);
+      } else {
+        throw `Transaction failed: ${getResponse}`;
+      }
+    }
+  }
 
   // @Timeout(0)
-  // async getTransactionsByLedger() {
-  //   try {
-  //     const data = await this.horizonServer
-  //       .transactions()
-  //       .forLedger(205963)
-  //       .call();
-  //     this.logger.log(JSON.stringify(data));
-  //   } catch (error) {
-  //     this.logger.error(error);
-  //   }
-  // }
+  async getTransactionsByAcconut() {
+    try {
+      const data = await this.horizonServer
+        .transactions()
+        .forAccount(ACCOUNT_ID)
+        .call();
+      this.logger.log(JSON.stringify(data));
+    } catch (error) {
+      this.logger.error(error);
+    }
+  }
 
   // @Timeout(0)
-  // async getEvents() {
-  //   while (true) {
-  //     try {
-  //       const startLedger = this.currentLedger;
-  //       const lastestLedger = await this.sorobanServer.getLatestLedger();
-  //       if (lastestLedger?.sequence >= startLedger + this.blockPerJob) {
-  //         const data = await this.sorobanServer.getEvents({
-  //           startLedger,
-  //           filters: [
-  //             {
-  //               type: 'contract',
-  //               contractIds: [CONTRACT_ID],
-  //             },
-  //           ],
-  //           limit: this.blockPerJob,
-  //         });
-  //         this.logger.log(
-  //           `Crawled from ${startLedger} to ${
-  //             startLedger + this.blockPerJob
-  //           } with data ${JSON.stringify(data)}`,
-  //         );
-  //         this.currentLedger = startLedger + this.blockPerJob;
-  //       } else {
-  //         this.logger.log(
-  //           `currentLedger: ${this.currentLedger}, lastestLedger: ${lastestLedger?.sequence}. Waiting for next job...`,
-  //         );
-  //         await new Promise((resolve) => setTimeout(resolve, 5000));
-  //       }
-  //     } catch (error) {
-  //       this.logger.error(error);
-  //       this.currentLedger += this.blockPerJob;
-  //     }
-  //   }
-  // }
+  async getTransactionsByLedger() {
+    try {
+      const data = await this.horizonServer
+        .transactions()
+        .forLedger(205963)
+        .cursor('now')
+        .call();
+      this.logger.log(JSON.stringify(data));
+    } catch (error) {
+      this.logger.error(error);
+    }
+  }
+
+  // @Timeout(0)
+  async getEvents() {
+    while (true) {
+      try {
+        const startLedger = this.currentLedger;
+        const lastestLedger = await this.sorobanServer.getLatestLedger();
+        if (lastestLedger?.sequence >= startLedger + this.blockPerJob) {
+          const data = await this.sorobanServer.getEvents({
+            startLedger,
+            filters: [
+              {
+                type: 'contract',
+                contractIds: [CONTRACT_ID],
+              },
+            ],
+            limit: this.blockPerJob,
+          });
+          this.logger.log(
+            `Crawled from ${startLedger} to ${
+              startLedger + this.blockPerJob
+            } with data ${JSON.stringify(data)}`,
+          );
+          this.currentLedger = startLedger + this.blockPerJob;
+        } else {
+          this.logger.log(
+            `currentLedger: ${this.currentLedger}, lastestLedger: ${lastestLedger?.sequence}. Waiting for next job...`,
+          );
+          await new Promise((resolve) => setTimeout(resolve, 5000));
+        }
+      } catch (error) {
+        this.logger.error(error);
+        this.currentLedger += this.blockPerJob;
+      }
+    }
+  }
 }
